66fdd0acd05f7b42c23bfb8a00f2e7f60a1f57f6
Kevin Liu <kevinliu314@gmail.com>
2018-07-09 18:29:00 -0500

    trying to figure out why my async message won't wokr

--- commandModules/reminder_background_checker.py
+++ commandModules/reminder_background_checker.py
@@ -7,7 +7,7 @@ import config
 class reminder_background_checker():
     def __init__(self, bot):
         self.bot = bot
-        self.notifier_bg_task = bot.loop.create_task(self.checker())
+        self.notifier_bg_task = self.bot.loop.create_task(self.checker())
     async def checker(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed:
@@ -15,11 +15,12 @@ class reminder_background_checker():
             r = handler.get_first_reminder()
             print(r)
             send_msg = handler.check_reminder(r)
+            await self.bot.say('fuck a duck')
             if send_msg:
                 print('inside if')
                 #delete reminder and send the message
                 # await client.send_message(r.user, r.message)
-                await self.bot.send_message(r.user, r.message)
+                # await self.bot.send_message(r.user, r.message)
                 print(r)
                 handler.delete_first_element()
             await asyncio.sleep(5)
