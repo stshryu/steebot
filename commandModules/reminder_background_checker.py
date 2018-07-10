60dcb85412058d220db50ef2b893c54a98dfa764
Kevin Liu <kevinliu314@gmail.com>
2018-07-10 01:49:26 -0500

    trying to figure out why async send wont work

--- commandModules/reminder_background_checker.py
+++ commandModules/reminder_background_checker.py
@@ -7,7 +7,8 @@ import config
 class reminder_background_checker():
     def __init__(self, bot):
         self.bot = bot
-        self.notifier_bg_task = self.bot.loop.create_task(self.checker())
+        # self.notifier_bg_task = self.bot.loop.create_task(self.checker())
+        self.bg_task = self.bot.loop.create_task(self.botTask())
     async def checker(self):
         await self.bot.wait_until_ready()
         while not self.bot.is_closed:
@@ -24,7 +25,16 @@ class reminder_background_checker():
                 print(r)
                 handler.delete_first_element()
             await asyncio.sleep(5)
-
+    async def botTask(self):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed:
+            print('hot second')
+            for attr in dir(self.bot):
+                if hasattr( self.bot, attr ):
+                    print( "bot.%s = %s" % (attr, getattr(self.bot, attr)))
+            # await self.bot.send_message(default_channel, "fuck a duck")
+            await asyncio.sleep(10)
+        print(self.bot.is_closed)
 
 def setup(bot):
     bot.add_cog(reminder_background_checker(bot))
